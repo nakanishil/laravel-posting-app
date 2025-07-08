f0ff5deff6fe986361cc9a4a6fff974f5adcd442
nakanishil <nakanishi.jsdf@gmail.com>
2025-07-08 09:47:28 +0900

    kadai 追加

--- laravel-posting-app/app/Models/User.php
+++ laravel-posting-app/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Post;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
